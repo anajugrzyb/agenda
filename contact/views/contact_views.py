d8b395b93f59e1d850a53c99aed1c028bf06c7e9
Ana Julia Grzyb <grzybanajulia8@gmail.com>
2025-07-10 18:54:05 -0300

    filtrando valores

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
+            )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': ' Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+    
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
