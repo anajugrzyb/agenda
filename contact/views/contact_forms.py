0623788b6dc202d5506703e93eb41e4b75c6e9ff
Ana Julia Grzyb <grzybanajulia8@gmail.com>
2025-07-21 14:21:15 -0300

    FORMS

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,38 +1,5 @@
-from typing import Any
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-
-from contact.models import Contact
-from django import forms
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact 
-        fields = (
-            'first_name', 'last_name', 'phone',
-            )
-        
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-        return super().clean()
-        
-    
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
