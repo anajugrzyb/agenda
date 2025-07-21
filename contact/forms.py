d0452f3d93b598f5f8d2bc5893a18e54025abc09
Ana Julia Grzyb <grzybanajulia8@gmail.com>
2025-07-21 14:53:07 -0300

    limpando o codigo

--- contact/forms.py
+++ contact/forms.py
@@ -16,25 +16,13 @@ class ContactForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        # self.fields['first_name'].widget.attrs.update({
-        #    'class':'classe-a classe-b',
-        #   'placeholder': 'Escreva aqui',           
-        # })
+
     class Meta:
         model = models.Contact 
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
             )
-        # widgets = {
-        #    'first_name': forms.TextInput(
-        #        attrs={
-        #           'class':'classe-a classe-b',
-        #           'placeholder': 'Escreva aqui',           
-        #        }
-        #    )
-        # }
         
     def clean(self):
         cleaned_data = self.cleaned_data
